8f7f652502542a08facbd233fece2907962e81f4
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-05-05 14:03:22 -0300

    Initial commit

---
+++ src/main/java/com/rodolfo/mongodbtest/MongodbTestApplication.java
@@ -0,0 +1,13 @@
+package com.rodolfo.mongodbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodolfo/mongodbtest/MongodbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolfo.mongodbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
